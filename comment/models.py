9fc1d1b0a30dafcc2d3fb5b5667c580bd3e97268
kunzhongzhou <kunzhongzhou@gmail.com>
2021-03-07 18:06:52 +0800

    Signed-off-by: kunzhongzhou <kunzhongzhou@gmail.com>

---
+++ comment/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+from article.models import ArticlePost
+from django.urls import reverse
+from ckeditor.fields import RichTextField
+from mptt.models import MPTTModel, TreeForeignKey
+
+# 博文的评论
+class Comment(MPTTModel):
+    article = models.ForeignKey(
+        ArticlePost,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+
+    # 新增，mptt树形结构
+    parent = TreeForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children'
+    )
+
+    # 新增，记录二级评论回复给谁, str
+    reply_to = models.ForeignKey(
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='replyers'
+    )
+
+    body = RichTextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['created']
+
+    def __str__(self):
+        return self.body[:20]
+
+
+    # 获取文章地址
+    def get_absolute_url(self):
+        return reverse('article:article_detail', args=[self.id])
---
+++ comment/tests.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+
+# Create your tests here.
+import datetime
+from django.utils import timezone
+from article.models import ArticlePost
+from django.contrib.auth.models import User
+
+
+class ArticlePostModelTests(TestCase):
+
+    def test_was_created_recently_with_future_article(self):
+        # 若文章创建时间为未来，返回 False
+        ...
+
+    def test_was_created_recently_with_seconds_before_article(self):
+        # 若文章创建时间为 1 分钟内，返回 True
+        author = User(username='user1', password='test_password')
+        author.save()
+        seconds_before_article = ArticlePost(
+            author=author,
+            title='test1',
+            body='test1',
+            created=timezone.now() - datetime.timedelta(seconds=45)
+            )
+        self.assertIs(seconds_before_article.was_created_recently(), True)
+
+    def test_was_created_recently_with_hours_before_article(self):
+        # 若文章创建时间为几小时前，返回 False
+        author = User(username='user2', password='test_password')
+        author.save()
+        hours_before_article = ArticlePost(
+            author=author,
+            title='test2',
+            body='test2',
+            created=timezone.now() - datetime.timedelta(hours=3)
+            )
+        self.assertIs(hours_before_article.was_created_recently(), False)
+
+    def test_was_created_recently_with_days_before_article(self):
+        # 若文章创建时间为几天前，返回 False
+        author = User(username='user3', password='test_password')
+        author.save()
+        months_before_article = ArticlePost(
+            author=author,
+            title='test3',
+            body='test3',
+            created=timezone.now() - datetime.timedelta(days=5)
+            )
+        self.assertIs(months_before_article.was_created_recently(), False)
